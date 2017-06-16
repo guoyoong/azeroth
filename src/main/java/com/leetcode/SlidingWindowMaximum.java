463b90058c3833b8b28c1f2fafa15901742d2404
guoyong1 <guoyong1@lenovo.com>
2017-06-16 12:05:10 +0800

    sliding window

--- src/main/java/com/leetcode/SlidingWindowMaximum.java
+++ src/main/java/com/leetcode/SlidingWindowMaximum.java
@@ -5,16 +5,15 @@ package com.leetcode;
 
 import org.junit.Test;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.TreeMap;
+import java.util.*;
 
 public class SlidingWindowMaximum {
 
     @Test
     public void test() {
-
+        int[] r = maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3);
+        for(int i : r)
+            System.out.println(i);
     }
 
     public int[] maxSlidingWindow2(int[] a, int k) {
@@ -30,10 +29,10 @@ public class SlidingWindowMaximum {
         }
         int[] r = new int[a.length - k + 1];
         for (int i = 0, j = k - 1; j < a.length; i++, j++) {
-            if (map.containsKey(a[i]))
-                map.put(a[i], map.get(a[i]) + 1);
+            if (map.containsKey(a[j]))
+                map.put(a[j], map.get(a[j]) + 1);
             else
-                map.put(a[i], 1);
+                map.put(a[j], 1);
             r[i] = map.lastKey();
             // 删除第一个元素
             Integer count = map.get(a[j - k + 1]);
@@ -82,5 +81,86 @@ public class SlidingWindowMaximum {
             if (i >= k - 1) max[j++] = nums[dequeue[pos]];
         }
         return max;
+    }
+
+    public String minWindow(String S, String T) {
+        int[] srcHash = new int[255];
+        // 记录目标字符串每个字母出现次数
+        for(int i = 0; i < T.length(); i++){
+            srcHash[T.charAt(i)]++;
+        }
+        int start = 0,i= 0;
+        // 用于记录窗口内每个字母出现次数
+        int[] destHash = new int[255];
+        int found = 0;
+        int begin = -1, end = S.length(), minLength = S.length();
+        for(start = i = 0; i < S.length(); i++){
+            // 每来一个字符给它的出现次数加1
+            destHash[S.charAt(i)]++;
+            // 如果加1后这个字符的数量不超过目标串中该字符的数量，则找到了一个匹配字符
+            if(destHash[S.charAt(i)] <= srcHash[S.charAt(i)]) found++;
+            // 如果找到的匹配字符数等于目标串长度，说明找到了一个符合要求的子串
+            if(found == T.length()){
+                // 将开头没用的都跳过，没用是指该字符出现次数超过了目标串中出现的次数，并把它们出现次数都减1
+                while(start < i && destHash[S.charAt(start)] > srcHash[S.charAt(start)]){
+                    destHash[S.charAt(start)]--;
+                    start++;
+                }
+                // 这时候start指向该子串开头的字母，判断该子串长度
+                if(i - start < minLength){
+                    minLength = i - start;
+                    begin = start;
+                    end = i;
+                }
+                // 把开头的这个匹配字符跳过，并将匹配字符数减1
+                destHash[S.charAt(start)]--;
+                found--;
+                // 子串起始位置加1，我们开始看下一个子串了
+                start++;
+            }
+        }
+        // 如果begin没有修改过，返回空
+        return begin == -1 ? "" : S.substring(begin,end + 1);
+    }
+
+
+    public String minWindow2(String S, String T) {
+        String res = "";
+        if(S == null || T == null || S.length()==0 || T.length()==0)
+            return res;
+
+        HashMap<Character, Integer> dict = new HashMap<>();
+        for(int i =0;i < T.length(); i++){
+            if(!dict.containsKey(T.charAt(i)))
+                dict.put(T.charAt(i), 1);
+            else
+                dict.put(T.charAt(i), dict.get(T.charAt(i))+1);
+        }
+
+        int count = 0;
+        int pre = 0;
+        int minLen = S.length()+1;
+        for(int i=0;i<S.length();i++){
+            if(dict.containsKey(S.charAt(i))){
+                dict.put(S.charAt(i),dict.get(S.charAt(i))-1);
+                if(dict.get(S.charAt(i)) >= 0)
+                    count++;
+                while(count == T.length()){
+                    if(dict.containsKey(S.charAt(pre))){
+                        dict.put(S.charAt(pre),dict.get(S.charAt(pre))+1);
+
+                        if(dict.get(S.charAt(pre))>0){
+                            if(minLen>i-pre+1){
+                                res = S.substring(pre,i+1);
+                                minLen = i-pre+1;
+                            }
+                            count--;
+                        }
+                    }
+                    pre++;
+                }
+            }//end for if(dict.containsKey(S.charAt(i)))
+        }
+        return res;
     }
 }
